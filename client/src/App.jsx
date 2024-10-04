cea45406469ad3dc13a670d128b2e58a4542c05e
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-04 13:14:11 -0600

    create primary pages and their routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      App 
-    </>
+    <BrowserRouter>
+      <h1 className="text-3xl">APP</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
