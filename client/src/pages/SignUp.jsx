f6338b83b1842fb89f77119eaefe80e75f4eedec
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-05 13:49:07 -0600

    create sign up jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import { Alert, Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const handleChange = (e) => {
+    console.log("handleChange");
+  };
+  const handleSubmit = () => {
+    console.log("handleSubmit");
+  };
   return (
-    <div>
-      Sign Up
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* Left Side  */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Jen's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a Blog Project for demos. You can sign in with your email or
+            Google!
+          </p>
+        </div>
+
+        {/* Right Side  */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your Username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              // disabled={loading}
+            >
+              {/* <>
+                <Spinner size="sm" />
+                <span className="pl-3">Loading...</span>
+              </> */}
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+          <Alert className="mt-5" color="failure">
+            {/* {errorMessage} */} errorMessage
+          </Alert>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
