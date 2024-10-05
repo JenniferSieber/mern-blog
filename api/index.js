0519f3bbc5b496e9631d51beae604945fd5c7fde
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-05 11:38:04 -0600

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-dotenv.config()
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connection to MongoDB successful."))
+    .catch(error => console.log('Error in DB connection', error));
 
 const app = express()
 const PORT = process.env.PORT  || 3001;
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+// API Middleware
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}.`));
