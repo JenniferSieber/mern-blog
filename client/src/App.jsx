91d11c30751757b4de39456519ecaf2d03480772
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-07 11:37:53 -0600

    privatize dashboard page with privateroute component functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import FooterComponent from './components/FooterComponent';
-import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Search from "./pages/Search";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
@@ -20,12 +21,15 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path='/search' element={<Search />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterComponent />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
