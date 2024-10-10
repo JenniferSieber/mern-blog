b4dc857043a2117644fc3c754a80af35081809cf
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-10 13:27:05 -0600

    update admin functionality on auth controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,10 +5,8 @@ import { errorHandler } from "../utils/error.js";
 
 // Signup Controller
 export const signup = async (req, res, next) => {
-  // console.log(req.body)
   const { username, email, password } = req.body;
-
-  // Our model has those requirements but this is extra step of securing the data before updating the database
+  // Our model has those reqs. -this is extra step of securing the data before updating the database
   if (
     !username ||
     username === "" ||
@@ -19,10 +17,8 @@ export const signup = async (req, res, next) => {
   ) {
     next(errorHandler(400, "All fields required for signup."));
   }
-
   // Hash password before passing to DB
   const hashedPassword = bcryptjs.hashSync(password, 10);
-
   const newUser = new User({
     username,
     email,
@@ -53,9 +49,11 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, "Invalid Credentials."));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
 
-    // return validUser sans password || hashedPassword
     const { password: pass, ...rest } = validUser._doc;
     res
       .status(200)
@@ -75,7 +73,10 @@ export const google = async (req, res, next) => {
     // User has already created account- signin process
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -102,11 +103,11 @@ export const google = async (req, res, next) => {
       });
       // new User saved to db
       await newUser.save();
-
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
-
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = newUser._doc;
-      // new User now signed in
       res
         .status(200)
         .cooke("access_token", token, {
@@ -118,6 +119,3 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
-
-// DELETE user
-export const deleteUser = (req, res, next) => {};
