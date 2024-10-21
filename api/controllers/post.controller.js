24b3e368d59bdd153972fe0d2fe1711c38633c4b
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-21 08:42:19 -0600

    create post API route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,15 +3,45 @@ import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
   console.log("create");
-  errorHandler();
-  const newPost = new Post();
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post."));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all required fields."));
+  }
+
+  if (!/^[a-zA-Z0-9\s]*$/.test(req.body.title)) {
+    return next(errorHandler(400, "Invalid post title: Only letters, numbers and spaces allowed."));
+  }
+
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    // .replace(/[^a-zA-Z0-9\s]/g, '');
+    
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
 };
+
 export const deletePost = async (req, res, next) => {
   console.log("deletePost");
 };
+
 export const getPosts = async (req, res, next) => {
   console.log("getPosts");
 };
+
 export const updatePost = async (req, res, next) => {
   console.log("updatePost");
 };
