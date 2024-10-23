cf69d6793867b10eea1304db69166f162de17807
Jennifer Sieber <jenwebdev2@gmail.com>
2024-10-23 13:53:28 -0600

    updating dashboard posts component

--- client/src/components/DashPostsComponent.jsx
+++ client/src/components/DashPostsComponent.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-function DashPostsComponent() {
+export default function DashPostsComponent() {
+  const { currentUser } = useSelector(state => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+  console.log(userPosts);
+  // will call ASYNC function 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+          if (data.posts.length < 0) {
+            setShowMore(false);
+          }
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id])
   return (
-    <div>
-      Dash Posts Component
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+
     </div>
   )
 }
 
-export default DashPostsComponent
+
